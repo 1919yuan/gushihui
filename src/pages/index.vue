cad7a394219304f5242fa6e24b3af76112ece589
Lin <1919yuan@gmail.com>
2020-04-18 12:22:51 -0400

    Update index with contact and about section, updated a few colors.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,25 +14,97 @@
       </div>
     </section>
     <section class="hero section2">
-      <div class="columns is-multiline">
+      <div class="columns is-multiline is-desktop">
         <div class="column is-12 has-text-centered">
-          <h2 class="title is-3">
+          <h2 class="title is-3 is-uppercase">
             {{ $t('landing.about') }}
           </h2>
         </div>
-        <div class="column is-6 has-text-right">
-          <p>asfasdfasdfasfa</p>
+        <div class="column is-offset-3 is-4 has-text-centered">
+          <p>
+            {{ $t('landing.about_paragraph') }}
+          </p>
         </div>
-        <div class="column is-6 has-text-left">
-          <figure><img src="~assets/icra.jpg"></figure>
+        <div class="column is-2 has-text-centered">
+          <figure>
+            <img src="~assets/icra.png" style="height: 10rem">
+          </figure>
         </div>
       </div>
     </section>
-    <section class="hero section3">
-      <div class="container has-text-centered">
-        <h2 class="title is-3">
-          Contact
-        </h2>
+    <section class="hero section3 is-success">
+      <h2 class="title is-3 is-uppercase has-text-centered">
+        {{ $t('landing.introduction') }}
+      </h2>
+      <div class="columns">
+        <div class="column">
+          <div class="notification is-warning">
+            <p class="has-text-centered">
+              <b-icon icon="calendar-week" size="is-large" />
+            </p>
+            <p>
+              {{ $t('landing.introduction_organize') }}
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="notification is-link">
+            <p class="has-text-centered">
+              <b-icon icon="account-multiple-plus" size="is-large" />
+            </p>
+            <p>
+              {{ $t('landing.introduction_register') }}
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="notification is-primary">
+            <p class="has-text-centered">
+              <b-icon icon="account-group" size="is-large" />
+            </p>
+            <p>
+              {{ $t('landing.introduction_manage') }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="hero section4 is-info">
+      <h2 class="title is-3 is-uppercase has-text-centered has-text-light">
+        {{ $t('landing.contact') }}
+      </h2>
+      <div class="columns is-multiline is-light">
+        <div class="column is-8 is-offset-2 is-light">
+          <form
+            action="https://formspree.io/email@example.com"
+            method="POST"
+          >
+            <div class="field">
+              <label> {{ $t('field.name') }} </label>
+              <b-input icon="account" name="Name" :placeholder="$t('ph.name')" />
+            </div>
+            <div class="field">
+              <label> {{ $t('field.email') }} </label>
+              <b-input
+                icon="email"
+                type="email"
+                :placeholder="$t('ph.email')"
+                name="Email"
+              />
+            </div>
+            <div class="field">
+              <label> {{ $t('field.message') }} </label>
+              <b-input
+                type="textarea"
+                :placeholder="$t('ph.message')"
+                name="Message"
+              />
+            </div>
+            <b-button class="is-block is-fullwidth" outlined>
+              {{ $t('button.submit') }}
+            </b-button>
+          </form>
+        </div>
       </div>
     </section>
     <Footer />
@@ -52,15 +124,23 @@ export default {
 </script>
 
 <style scoped lang="sass">
-@import "~bulma/sass/utilities/_all";
+@import "@/styles/variables.sass";
 .section1
-  backgrounpd: url('~assets/banner.jpg') center / cover
+  background: url('~assets/banner.jpg') center / cover
 .section2
   background: $light
-  padding: 5rem
-.section2 .title
-  text-transform: uppercase;
+  padding: 1rem
++desktop
+  .section2
+    padding: 5rem
 .section3
-  background: $grey-lighter
-  padding: 5rem
+  padding: 1rem
++desktop
+  .section3
+    padding: 5rem
+.section4
+  padding: 1rem
++desktop
+  .section4
+    padding: 5rem
 </style>
